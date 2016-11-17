264c8b222ea1575562c67f283edada2cfa46a8b4
Yongbin <happye321@gmail.com>
2016-11-17 10:19:47 -0700

    version 0.01

---
+++ blib/lib/NextBio/NextBio.pl
@@ -0,0 +1,189 @@
+#!/usr/bin/perl -w
+use strict;
+use NextBio::Utilities;
+use Getopt::Long;
+my  %opts;
+my  $handle;
+GetOptions(
+		   'help'	    =>\$opts{help},
+		   'function=s' =>\$opts{function},
+		   'fastq=s'    =>\$opts{fastq},
+		   'fasta=s'    =>\$opts{fasta},
+		   'header=s'   =>\$opts{header},
+		   'order=s'    =>\$opts{order},
+		   'length=i'   =>\$opts{length},
+		   'list=s'		=>\$opts{list},
+		   'phy=s'		=>\$opts{phy},
+		   'overhang=s'	=>\$opts{overhang},
+		   'threshold=f'  =>\$opts{threshold},
+
+		  );
+ 
+
+if($opts{help} or !defined $opts{function})
+{
+ 	print "\n There is somthing wrong with your command line !!!!\n";
+ 	print &help;
+}
+elsif($opts{function} eq 'Fastq_uniq')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fastq_uniq($opts{fastq})
+}
+
+elsif($opts{function} eq 'Fasta_uniq')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fasta_uniq($opts{fasta},$opts{header})
+}
+
+elsif($opts{function} eq 'Fasta_sort')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fasta_sort($opts{fasta},$opts{order})
+}
+
+elsif($opts{function} eq 'Fasta_length')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fasta_length($opts{fasta},$opts{length},$opts{order})
+}
+
+elsif($opts{function} eq 'Fasta_share')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fasta_share($opts{list})
+}
+
+elsif($opts{function} eq 'Fasta_extract')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fasta_extract($opts{fasta},$opts{list})
+}
+
+elsif($opts{function} eq 'Fasta_exclude')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fasta_exclude($opts{fasta},$opts{list})
+}
+
+elsif($opts{function} eq 'Fastq2Fasta')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->Fastq2Fasta($opts{fastq})
+}
+
+elsif($opts{function} eq 'phy_clean')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->phy_clean($opts{phy},$opts{threshold})
+}
+
+elsif($opts{function} eq 'overhang_check')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->overhang_check($opts{fastq},$opts{overhang})
+}
+
+elsif($opts{function} eq 'file_rename')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->file_rename($opts{list})
+}
+
+elsif($opts{function} eq 'N50_count')
+{
+	$handle=NextBio::Utilities->new();
+	$handle->N50_count($opts{fasta})
+}
+
+
+sub help
+{ 
+my $usage=	
+"=========================================================================
+--help 		print this usage
+--function	select function to use
+--fastq 	fastq file to process
+--fasta		fasta file to process
+--header	string used to modify fasta header, use with fasta_uniq
+--order		up or down used with fasta_sort, default is upward
+--length    	read length less than specified value will be removed
+--list		Contains list of file names or fasta header
+--phy 		.phy file to process
+--overhang 	enzyme overhang you want to detect on 5'
+--threshold 	floating point value of missing data allowed for samples (default 0.9999)
+
+Deamon uage:
+#get help message 
+./Fastx_Utilites.pl --help
+
+#get uniq fastq file
+./Fastx_Utilites.pl --Function Fastq_uniq --fastq test.fq
+
+#get uniq fasta file and rename header
+./Fastx_Utilites.pl --Function Fasta_uniq --fasta test.fa --header Mine
+
+#sort fasta file based on their length
+./Fastx_Utilites.pl --function Fasta_sort --fasta test.fa --order down
+
+#filter fasta file based on length
+./Fastx_Utilites.pl --function Fasta_length --fasta test.fa --length 10
+
+#extract fasta file by providing a list of header ID (no >)
+./Fastx_Utilites.pl --function Fasta_extract --fasta test.fa --list
+
+#exclude entries from fasta file by providing a list of header ID (no >)
+./Fastx_Utilites.pl --function Fasta_exclude --fasta test.fa --list
+
+#transform fastq to fasta
+./Fastx_Utilites.pl --function Fastq2Fasta --fastq test.fq
+
+#find sequences shared by multiple fasta file, by providing a list of file names, each filename on one line
+./Fastx_Utilites.pl --function Fasta_share --list list.txt
+
+#remove samples contains only Ns and sort the samples based on number of informative bases
+./Fastx_Utilites.pl --function phy_clean --phy ../Pcp.phy --threshold 0.8 >../Pcp_60_cleaned.phy 
+
+#remove sequences not start with expected enzyme cutting overhang
+./Fastx_Utilites.pl --function overhang_check --fastq test.q --overhang TTCA
+
+#rename files in current dir, format for list 'currentname  newname', no space is allowed in the file names
+./Fastx_Utilites.pl --function file_rename --list list.txt
+=========================================================================
+
+";
+	print $usage;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ blib/lib/NextBio/Utilities.pm
@@ -0,0 +1,548 @@
+package NextBio::Utilities;
+
+use 5.018002;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use NextBio::Utilities ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+sub new
+{
+	my $self={};
+	bless($self);
+	return($self);
+}
+
+# Preloaded methods go here.
+############################
+sub Fastq_uniq
+{
+	shift;
+	my $file=shift;
+	open(FILE,"$file");
+	my @content=<FILE>;
+	my $line;
+	my $count;
+	my %bash;
+	for($count=1;$count<=($#content-2);$count+=4)
+	{
+		$bash{$content[$count]}=$content[$count-1].$content[$count].$content[$count+1].$content[$count+2]
+	}
+	foreach my $line(keys %bash)
+	{
+		print $bash{$line}
+	}
+	close FILE;
+}
+
+
+############################
+sub Fasta_uniq
+{
+	shift;
+	my $file=shift;
+	my $rename=shift;
+	open(FILE,"$file");
+	my @temp=<FILE>;
+	my ($line,$count);
+	my %bash;
+	my @content;
+	$count=0;
+	foreach $line(@temp)
+	{
+		chomp $line;
+		if($line=~/>/)
+		{
+			unless ($count==0)
+			{
+				$count++
+			}
+			$content[$count]=$line;
+			$count++;
+		}
+		else
+		{
+			if(defined $content[$count])
+			{
+				$content[$count]=$content[$count].$line;
+			}
+			else
+			{
+				$content[$count]=$line;
+			}
+		}
+
+	}
+
+	for($count=1;$count<=($#content);$count+=2)
+	{
+		$bash{$content[$count]}=$content[$count-1]
+	}
+	$count=1;
+	if($rename)
+	{
+		foreach $line(keys %bash)
+		{
+			print ">",$rename,"_seq_",$count,"\n",$line,"\n";
+			$count++
+		}
+	}	
+	else
+	{
+		foreach $line(keys %bash)
+		{
+			
+			print $bash{$line},"\n";
+			$line=~s/(.{60})/$1\n/g;
+			print $line,"\n";
+		}
+	}
+	close FILE;
+}
+
+
+############################
+sub Fasta_sort
+{
+	shift;
+	my $file=shift;
+	my $order=shift;
+	open(FILE,"$file");
+	my @temp=<FILE>;
+	my %hash;
+	my ($key,$line);
+	my @content;
+	my $count=0;
+	foreach $line(@temp)
+	{
+		chomp $line;
+		if($line=~/>/)
+		{
+			unless ($count==0)
+			{
+				$count++
+			}
+			$content[$count]=$line;
+			$count++;
+		}
+		else
+		{
+			if(defined $content[$count])
+			{
+				$content[$count]=$content[$count].$line;
+			}
+			else
+			{
+				$content[$count]=$line;
+			}
+		}
+
+	}
+	for(my $count=0;$count<$#content;$count+=2)
+	{
+		my $length=length($content[$count+1]);
+		$content[$count+1]=~s/(.{60})/$1\n/g;
+		my $temp=$content[$count]."\n".$content[$count+1]."\n";
+		$hash{$temp}=$length;
+	}
+		
+	if(defined $order eq 'down')
+	{
+		foreach $key(sort{ $hash{$b} <=> $hash{$a}}keys %hash)
+		{
+			print $key;
+		}
+	}
+	else
+	{
+		foreach $key(sort{ $hash{$a} <=> $hash{$b}}keys %hash)
+		{
+			print $key;
+		}	
+	}
+	close FILE;
+}
+
+
+############################
+sub Fasta_length
+{
+	shift;
+	my $file=shift;
+	my $length=shift;
+	my $order=shift;
+	open(FILE,"$file");
+	my @temp=<FILE>;
+	my %hash;
+	my $key;
+	my @content;
+	my $count=0;
+	foreach my $line(@temp)
+	{
+		chomp $line;
+		if($line=~/>/)
+		{
+			unless ($count==0)
+			{
+				$count++
+			}
+			$content[$count]=$line;
+			$count++;
+		}
+		else
+		{
+			if(defined $content[$count])
+			{
+				$content[$count]=$content[$count].$line;
+			}
+			else
+			{
+				$content[$count]=$line;
+			}
+		}
+
+	}
+		for(my $count=0;$count<$#content;$count+=2)
+	{
+		my $leng=length($content[$count+1]);
+		$content[$count+1]=~s/(.{60})/$1\n/g;
+		my $temp=$content[$count]."\n".$content[$count+1]."\n";
+		$hash{$temp}=$leng if $leng >= $length;
+	}
+		
+	if(defined $order eq 'down')
+	{
+		foreach $key(sort{ $hash{$b} <=> $hash{$a}}keys %hash)
+		{
+			print $key;
+		}
+	}
+	else
+	{
+		foreach $key(sort{ $hash{$a} <=> $hash{$b}}keys %hash)
+		{
+			print $key;
+		}	
+	}
+	close FILE;
+}
+
+
+
+############################
+# make sure all fasta file is uniq
+sub Fasta_share
+{
+	shift;
+	my $list=shift;
+	open(LIST,"$list");
+	my @file=<LIST>;
+	my $number=@file;
+	my %hash;
+	foreach my $line(@file)
+	{
+		chomp $line;
+		open(INPUT,"$line");
+		my @temp=<INPUT>;
+		for(my $count=0;$count<$#temp;$count+=2)
+		{
+			if($hash{$temp[$count+1]}) 
+			{
+				$hash{$temp[$count+1]}=$hash{$temp[$count+1]}+1;
+			}
+			else
+			{
+				$hash{$temp[$count+1]}=1;
+			}
+		}
+		close INPUT;
+	}
+	my $header=1;
+	foreach my $line(keys %hash)
+	{
+		if($hash{$line} == $number)
+		{
+			print ">shared_seq",$header,"\n",$line;
+			$header++;
+		}
+	}
+	close LIST;
+	
+}
+
+
+############################
+sub Fasta_extract
+{
+	shift;
+	my $fasta=shift;
+	my $list=shift;
+	open(FASTA,"$fasta");
+	open(LIST,"$list");
+	my @fasta=<FASTA>;
+	my @list=<LIST>;
+	my %hash;
+	for(my $count=0;$count<$#fasta;$count+=2)
+	{
+		chomp $fasta[$count];
+		$fasta[$count]=~/>(.+)/;
+		my $temp=$fasta[$count]."\n".$fasta[$count+1];
+		$hash{$1}=$temp
+	}
+	foreach my $line(@list)
+	{
+		chomp $line;
+		if($hash{$line})
+		{
+			print $hash{$line}
+		}
+	}
+	close FASTA;
+	close LIST;
+
+}
+
+
+############################
+sub Fasta_exclude
+{
+	shift;
+	my $fasta=shift;
+	my $list=shift;
+	open(FASTA,"$fasta");
+	open(LIST,"$list");
+	my @fasta=<FASTA>;
+	my @list=<LIST>;
+	my %hash;
+	for(my $count=0;$count<$#fasta;$count+=2)
+	{
+		chomp $fasta[$count];
+		$fasta[$count]=~/>(.+)/;
+		my $temp=$fasta[$count]."\n".$fasta[$count+1];
+		$hash{$1}=$temp
+	}
+	foreach my $line(@list)
+	{
+		chomp $line;
+		unless($hash{$line})
+		{
+			print $hash{$line}
+		}
+	}
+	close FASTA;
+	close LIST;
+
+}
+
+
+############################
+sub Fastq2Fasta
+{
+	shift;
+	my $fastq=shift;
+	open(FASTQ,"$fastq");
+	my $count=3;
+	my $name=1;
+	foreach my $line (<FASTQ>)
+	{
+		if($count%4==0)
+		{
+			print ">seq_",$name,"\n";
+			print $line;
+			$name++;
+		}
+		$count+=1;
+	}	
+	close FASTQ;
+}
+
+
+###########################
+sub Fasta_stastic
+{
+
+}
+
+
+###########################
+sub phy_clean
+{
+	shift;
+	my $phy=shift;
+	my $threshold=shift;
+	unless(defined $threshold)
+	{
+		$threshold=0;
+	}else
+	{
+		$threshold=1-$threshold;
+	}
+	open(PHY,$phy);
+	my @phy=(<PHY>);
+	my %hash1;
+	my %hash2;
+	my @loci=split(" ",$phy[0]);
+	for(my $count=1;$count<=$#phy;$count++)
+	{
+		my $nucleo_count=$phy[$count]=~tr/ACTG/ATCG/;
+		$phy[$count]=~/(.+?)\s+(.+)/;
+		my $name=$1;
+		my $seq=$2;
+		my $N=$seq=~tr/ACTG/ACTG/;
+		unless ($N <= $threshold * $loci[1])
+		{
+			$hash1{$name}=$phy[$count];
+			$hash2{$name}=$nucleo_count;
+		}
+	}
+	my $samples=scalar keys %hash1;
+	print $samples," ",$loci[1],"\n";
+	foreach my $key (sort{ $hash2{$b} <=> $hash2{$a} } keys %hash2)
+	{
+		print $hash1{$key};
+	}
+	close PHY;
+}
+
+
+###########################
+sub overhang_check
+{
+	shift;
+	my $file=shift;
+	my $overhang=shift;
+	open(FILE,"$file");
+	my @content=<FILE>;
+	my $line;
+	my $count;
+	my %bash;
+	for($count=1;$count<=($#content-2);$count+=4)
+	{
+		$bash{$content[$count]}=$content[$count-1].$content[$count].$content[$count+1].$content[$count+2];
+		print $bash{$content[$count]} if $content[$count]=~/^$overhang/
+	}
+	close FILE;
+}
+###########################
+sub file_rename
+{
+	shift;
+	my $list=shift;
+	open(LIST,"$list");
+	for my $line(<LIST>)
+	{
+		my @name=split(" ",$line);
+		`mv $name[0] $name[1]`
+	}
+	close LIST;
+}
+###########################
+sub N50_count
+{
+	shift;
+	my $file=shift;
+	open(FILE,"$file");
+	my @content=<FILE>;
+	my %hash;
+	my $key;
+	my $sum=0;
+	my $N50=0;
+	for(my $count=0;$count<$#content;$count+=2)
+	{
+		my $temp=$content[$count].$content[$count+1];
+		$hash{$temp}=length($content[$count+1]);
+		$sum+=$hash{$temp}
+	}
+		
+	foreach $key(sort{ $hash{$a} <=> $hash{$b}}keys %hash)
+	{
+		$N50+=$hash{$key};
+		if($N50 > $sum/2)
+		{
+			print "N50 is: $hash{$key}\n";
+			last;
+		}
+	}
+	
+	close FILE;
+}
+
+###########################
+
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+NextBio::Utilities - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use NextBio::Utilities;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for NextBio::Utilities, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Yongbin, E<lt>Yongbin@apple.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 by Yongbin
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
