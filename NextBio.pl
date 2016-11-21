56e77fb6b1b3b73152683f6b3aaa20a1f1216e93
Yongbin <happye321@gmail.com>
2016-11-21 13:18:22 -0700

    update

--- NextBio.pl
+++ NextBio.pl
@@ -117,59 +117,60 @@ my $usage=
 
 Deamon uage:
 #################################  Build-in function ##############################
-#get help message 
+# get help message 
 ./NextBio.pl --help
 
-#get uniq fastq file
+# get uniq fastq file
 ./NextBio.pl --Function Fastq_uniq --fastq test.fq
 
-#get uniq fasta file and rename header
+# get uniq fasta file and rename header
 ./NextBio.pl --Function Fasta_uniq --fasta test.fa --header Mine
 
-#sort fasta file based on their length
+# sort fasta file based on their length
 ./NextBio.pl --function Fasta_sort --fasta test.fa --order down
 
-#filter fasta file based on length
+# filter fasta file based on length
 ./NextBio.pl --function Fasta_length --fasta test.fa --length 10
 
-#extract fasta file by providing a list of header ID (no >)
+# extract fasta file by providing a list of header ID (no >)
 ./NextBio.pl --function Fasta_extract --fasta test.fa --list
 
-#exclude entries from fasta file by providing a list of header ID (no >)
+# exclude entries from fasta file by providing a list of header ID (no >)
 ./NextBio.pl --function Fasta_exclude --fasta test.fa --list
 
-#transform fastq to fasta
+# transform fastq to fasta
 ./NextBio.pl --function Fastq2Fasta --fastq test.fq
 
-#find sequences shared by multiple fasta file, by providing a list of file names, each filename on one line
+# find sequences shared by multiple fasta file, by providing a list of file names, each filename on one line
 ./NextBio.pl --function Fasta_share --list list.txt
 
-#remove samples contains only Ns and sort the samples based on number of informative bases
+# remove samples contains only Ns and sort the samples based on number of informative bases
 ./NextBio.pl --function phy_clean --phy ../Pcp.phy --threshold 0.8 >../Pcp_60_cleaned.phy 
 
-#remove sequences not start with expected enzyme cutting overhang in fastq file
+# remove sequences not start with expected enzyme cutting overhang in fastq file
 ./NextBio.pl --function overhang_check --fastq test.q --overhang TTCA
 
-#rename files in current dir, format for list 'currentname	newname', seperate by tab
+# rename files in current dir, format for list 'currentname	newname', seperate by tab
 ./NextBio.pl --function file_rename --list list.
 
-#calculate cotig N50
+# calculate cotig N50
 ./NextBio.pl --function N50_count --fasta test.fa
 
-#translate DNA into protein with all six reading file_rename
+# translate DNA into protein with all six reading file_rename
 ./NextBio.pl --function translate --fasta test.fa
 
 
 ################################# Function requires dependency ####################
 ****###########################  R, samtools, bcftools, bowtie2####################
 
-#ploidy_plot analysis: single end read
-#stage1:buiding bowtie2 index
-#stage2:generate mapping file
-#stage3:generating vcf file and making the plot 
-#you can change depth value and just run step3 to see the difference
+# ploidy_plot
+# stage1:buiding bowtie2 index
+# stage2:generate mapping file
+# stage3:generating vcf file and making the plot 
+# you can change depth value and just run step3 to see the difference
+# ploidy_plot analysis: single end read
 ./NextBio.pl --function ploidy --fasta ref.fa --fastq test.fq --depth 30 --header test --stage 3
-#ploidy_plot analysis: paired end reads
+# ploidy_plot analysis: paired end reads
 ./NextBio.pl --function ploidy --fasta ref.fa --fastq test1.fq test2.fq --depth 30 --header test  --stage 3
 
 
